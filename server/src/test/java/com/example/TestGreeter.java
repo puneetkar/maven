7eefc35469834a981f027f6abd8a23744112853b
PUNEET <34155834+puneetkar@users.noreply.github.com>
2025-10-22 16:45:27 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -1,33 +1,26 @@
 package com.example;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class TestGreeter {
 
-  private Greeter greeter;
+    private Greeter greeter;
 
-  @Before
-  public void setup() {
-    greeter = new Greeter();
-  }
+    @BeforeEach
+    void setUp() {
+        greeter = new Greeter();
+    }
 
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    @Test
+    void testGreetingContainsHello() {
+        assertThat(greeter.greet("World"), containsString("Hello"));
+    }
 
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
+    @Test
+    void testGreetingLength() {
+        assertThat(greeter.greet("World").length(), greaterThan(5));
+    }
 }
